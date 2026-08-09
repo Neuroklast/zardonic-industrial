81a7d41908e82c3e23fef4340de240606e727af1
Kay Schäfer <info@neuroklast.net>
2026-08-09 21:26:42 +0200

    chore: drop unused Image import in BackgroundConfigEditor

--- app/admin/(protected)/site-config/BackgroundConfigEditor.tsx
+++ app/admin/(protected)/site-config/BackgroundConfigEditor.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useMemo, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { broadcastAdminDraft } from '@/lib/admin-draft-channel'
-import Image from 'next/image'
 import { updateSiteConfig } from '@/app/admin/_actions/siteConfig'
 import { MediaSourcePicker } from '@/app/admin/_components/MediaSourcePicker'
 import { VideoSourcePicker } from '@/app/admin/_components/VideoSourcePicker'
